7b61ca561bd73711dc6b070def30b12150032d78
Michael Hathaway <mikehath5@gmail.com>
2021-03-03 12:04:17 -0800

    Built main App component and added styling.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useCountriesData from '../hooks/useCountriesData';
+import useSelectedCountryDetails from '../hooks/useSelectedCountryDetails';
+import useSelectedCountryTimeline from '../hooks/useSelectedCountryTimeline';
+import Header from './Header';
+import Card from './Card';
+import CovidCasesInfoBox from './CovidCasesInfoBox';
+import Map from './Map';
+import Table from './Table';
+import CovidDataLineGraph from './CovidDataLineGraph';
+import '../style/app.css';
 
 const App = () => {
-  return <div>This is a COVID-19 Tracker</div>;
+  const countries = useCountriesData();
+  const [selectedCountry, setSelectedCountry] = useState('Worldwide');
+  const countryData = useSelectedCountryDetails(selectedCountry);
+  const timelineData = useSelectedCountryTimeline(selectedCountry);
+
+  return (
+    <div className="app">
+      <div className="app__top">
+        <Header
+          countries={countries}
+          selectedCountry={selectedCountry}
+          setSelectedCountry={setSelectedCountry}
+        />
+        <div className="app__stats">
+          <Card className="app__stats__container">
+            <CovidCasesInfoBox
+              title="Coronavirus Cases"
+              total={countryData.cases}
+              cases={countryData.todayCases}
+            />
+            <CovidDataLineGraph
+              country={selectedCountry}
+              data={timelineData}
+              type="cases"
+            />
+          </Card>
+          <Card className="app__stats__container">
+            <CovidCasesInfoBox
+              title="Recovered"
+              total={countryData.recovered}
+              cases={countryData.todayRecovered}
+            />
+            <CovidDataLineGraph
+              country={selectedCountry}
+              data={timelineData}
+              type="recovered"
+            />
+          </Card>
+          <Card className="app__stats__container">
+            <CovidCasesInfoBox
+              title="Deaths"
+              total={countryData.deaths}
+              cases={countryData.todayDeaths}
+            />
+            <CovidDataLineGraph
+              country={selectedCountry}
+              data={timelineData}
+              type="deaths"
+            />
+          </Card>
+        </div>
+      </div>
+
+      <Card className="app__bottom">
+        <div className="app__bottom__left"></div>
+        <div className="app__bottom__right">
+          <h3>Active Cases By Country</h3>
+          <Table countries={countries} />
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './style/index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
